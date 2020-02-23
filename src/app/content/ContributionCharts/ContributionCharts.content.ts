e1296431ca427b6e38f87c95768c8b00439438fc
laurentlaurent <lao.laurent@gmail.com>
2020-02-23 14:51:40 -0500

    Rename Github contribution chart

--- src/app/content/ContributionCharts/ContributionCharts.content.ts
+++ src/app/content/ContributionCharts/ContributionCharts.content.ts
@@ -2,7 +2,7 @@ import {ContributionChart} from '../../interface-types/contribution-chart';
 
 export const CONTRIBUTION_CHARTS: ContributionChart[] = [
   {
-    theSectionName: 'GitHub Calendar',
+    theSectionName: 'GitHub Contribution Chart',
     theProfileLink: 'https://github.com/laurentlaurent',
     theChartAPI: '<img src="http://ghchart.rshah.org/laurentlaurent" alt="laurentlaurent\'s github chart"/>',
     theChartCredit: 'Github Calendar from rshah. (Github link: https://github.com/2016rshah/githubchart-api)'
